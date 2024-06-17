627590a31779d069bc1b66643ca772a8140cedae
Ivan Berrutto <ivanberrutto@gmail.com>
2024-06-17 18:31:06 -0300

    resuelvo reverse y reorganizo archivos

--- Guia 7/ejercicio4.pl
+++ Guia 7/ejercicio4.pl
@@ -2,3 +2,5 @@
 
 juntar([],Lista2,Lista2)
 juntar([X|T1],Lista2,[X|T3]) :- juntar(T1,Lista2,T3)
+
+
---
+++ Guia 7/ejercicio5.pl
@@ -0,0 +1,14 @@
+%% last(?L, ?U), donde U es el último elemento de la lista L
+last([ X ], X).
+last((_|T),Y) :- last(T,Y).
+
+%mismaLongitud (+L,+L1)
+mismaLongitud(L,L1) :- length(L,N) , length(L1,N).
+
+%% reverse(+L, -L1), donde L1 contiene los mismos elementos que L, pero en orden inverso.
+reverse([],[]).
+reverse([X|XS],Y) :- mismaLongitud([X|XS],Y), reverse(XS,Z), append(Z, [X] , Y).
+
+
+%% maxlista(+L , -M) , minlista(+L, -M) donde M es el maximo/minimo de cada lista.
+
